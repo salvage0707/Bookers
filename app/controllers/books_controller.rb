286dd79a59685929cca4b3e2adca3d69d3024cb6
Masaki Ikegami <salvage0707@gmail.com>
2018-04-05 20:35:57 +0900

    make top index new

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,28 @@
+class BooksController < ApplicationController
+
+	def top
+  end
+
+  def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	book.save
+  	redirect_to books_path
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+
+  private
+
+  	def book_params
+  		params.require(:book).permit(:title, :body)
+  	end
+
+end
