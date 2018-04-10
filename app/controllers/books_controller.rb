53b9a7711d5ac5786f60f54f2eebfb183ac7e3e8
Masaki Ikegami <salvage0707@gmail.com>
2018-04-10 16:03:53 +0900

    remake

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
   def create
   	book = Book.new(book_params)
   	book.save
-  	redirect_to books_path
+  	redirect_to book_path(book.id)
   end
 
   def show
